11387f56571070892e5d949ed8f0b52bf5e081fb
tapaswin-g <tapaswin.g@gmail.com>
2025-11-14 09:30:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,8 @@ if ingredients_list:
 
   
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response.json())
+    # st.text(smoothiefroot_response.json())
+    sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
   
 
     #st.write(my_insert_stmt)
